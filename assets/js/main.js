cf406df06911ccb7adcd6bdf7c7332ebf9f018b9
petergiromagny <peter.giromagny@viacesi.fr>
2020-03-02 14:04:59 +0100

    Add/Update Portfolio project and image + resume download function

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,42 @@ function scrollNav() {
     $('.scrollTop a').scrollTop();
 }
 scrollNav();*/
+
+/*
+Resume Download
+ */
+
+/*
+$('a#resume').attr({target: '_blank',
+    href  : 'assets/resume.pdf'});*/
+
+function download_file(fileURL, fileName) {
+    // for non-IE
+    if (!window.ActiveXObject) {
+        var save = document.createElement('a');
+        save.href = fileURL;
+        save.target = '_blank';
+        var filename = fileURL.substring(fileURL.lastIndexOf('/')+1);
+        save.download = fileName || filename;
+        if ( navigator.userAgent.toLowerCase().match(/(ipad|iphone|safari)/) && navigator.userAgent.search("Chrome") < 0) {
+            document.location = save.href;
+// window event not working here
+        }else{
+            var evt = new MouseEvent('click', {
+                'view': window,
+                'bubbles': true,
+                'cancelable': false
+            });
+            save.dispatchEvent(evt);
+            (window.URL || window.webkitURL).revokeObjectURL(save.href);
+        }
+    }
+
+    // for IE < 11
+    else if ( !! window.ActiveXObject && document.execCommand)     {
+        var _window = window.open(fileURL, '_blank');
+        _window.document.close();
+        _window.document.execCommand('SaveAs', true, fileName || fileURL)
+        _window.close();
+    }
+}
