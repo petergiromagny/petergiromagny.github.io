e6736f4cbed2d1a0944413d7b369c0bf5d743d91
Peter <peter.giromagny@me.com>
2020-04-08 19:07:22 +0200

    Create popup modal Portfolio + add img

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,68 +1,6 @@
-/*--------------nav link active--------------*/
-
-/*// Get the container element
-var btnContainer = document.getElementById("link");
-
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("link-btn");
-
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-
-        // If there's no active class
-        if (current.length > 0) {
-            current[0].className = current[0].className.replace(" active", "");
-        }
-
-        // Add the active class to the current/clicked button
-        this.className += " active";
-    });
-}*/
-
-/*--------------more information about me active--------------*/
-
-// Get the modal
-/*var modal = document.getElementById("modal-more");
-var btn = document.getElementById("btn-information");
-var close = document.getElementById("btn-close");
-
-btn.onclick = function() {
-    modal.style.display = "initial";
-    btn.style.display = "none";
-    close.style.display = "initial"
-};
-
-close.onclick = function() {
-    modal.style.display = "none";
-    close.style.display = "none";
-    btn.style.display = "initial"
-};*/
-
-/*--------------Burger Menu--------------*/
-
 /*
-function scrollNav() {
-    $('.nav a').click(function(){
-        //Animate
-        $('html, body').stop().animate({
-            scrollTop: $( $(this).attr('href') ).offset().top - 160
-        }, 400);
-        return false;
-    });
-    $('.scrollTop a').scrollTop();
-}
-scrollNav();*/
-
-/*
-Resume Download
- */
-
-/*
-$('a#resume').attr({target: '_blank',
-    href  : 'assets/resume.pdf'});*/
-
+* Download CV
+*/
 function download_file(fileURL, fileName) {
     // for non-IE
     if (!window.ActiveXObject) {
@@ -92,4 +30,33 @@ function download_file(fileURL, fileName) {
         _window.document.execCommand('SaveAs', true, fileName || fileURL)
         _window.close();
     }
+}
+
+/*
+* Open modal in pop up
+*/
+// Get the modal
+var modal = document.getElementById("imgModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btnModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+	modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target === modal) {
+		modal.style.display = "none";
+	}
 }
