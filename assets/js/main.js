5d8da2f33d106ae9dfe6f53456cba27c9a932bce
Peter <peter.giromagny@me.com>
2020-04-09 11:19:41 +0200

    Update popup modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,28 +36,31 @@ function download_file(fileURL, fileName) {
 * Open modal in pop up
 */
 // Get the modal
-var modal = document.getElementById("imgModal");
-var body = document.getElementById("body-section");
+let modal = document.getElementById("imgModal");
+let body = document.querySelector('body');
 
 // Get the button that opens the modal
-var btn = document.getElementById("btnModal");
+let btn = document.getElementById("btnModal");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
+	body.classList.add('no-scroll');
 	modal.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+	body.classList.remove('no-scroll');
 	modal.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
 	if (event.target === modal) {
+		body.classList.remove('no-scroll');
 		modal.style.display = "none";
 	}
 };
